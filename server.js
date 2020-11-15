4bb583ce36eb56596efdb2105bb054e21ccc4632
Ethan Torres <torresethan99@gmail.com>
2020-11-14 21:13:13 -0600

    added session functionality to the server file

--- server.js
+++ server.js
@@ -3,7 +3,26 @@ const express = require('express');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({
+  helpers
+});
+
+
+const session = require('express-session');
+const  SequelizeStore = require('connect-session-sequelize')(session.store);
+
+const sess = {
+  secret: process.env.DB_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+    checkExpirationInterval: 1000 * 60 * 10, //checks every 10 minutes
+    expiration: 1000 * 60 * 30 //expires after 30 minutes
+  })
+};
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -12,11 +31,12 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: true }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}!`));
 });
