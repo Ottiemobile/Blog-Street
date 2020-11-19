28b058bae3c5c6bc29e5892e958fa203755707af
Ethan Torres <torresethan99@gmail.com>
2020-11-18 20:11:14 -0600

    need help for login page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -16,6 +16,9 @@ router.get('/', (req, res) => {
             {
                 model: Comment,
                 attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+  
+  
+  
                 include: {
                     model: User,
                     attributes: ['username']
@@ -95,7 +98,7 @@ router.get('/post/:id', (req,res) => {
 
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
-        res.redirect('/');
+        res.render('login');
     }
 });
 
